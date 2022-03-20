bcd80093e4ef74417229b852e6e1ff68a3f8bbfa
Maksim Sychev <maksim_sychev@epam.com>
2022-03-20 18:10:29 +0300

    1
    1

--- router/user-router.js
+++ router/user-router.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const {usersValidator} = require('../api/validation');
-const db = require('../db/db.js');
 const {authUser,registerUser} = require('../controllers/user-controller')
 
 const userRouter = express.Router();
