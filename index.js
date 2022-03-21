5840c66e16d979030aabda3700f92a80af7e5123
Maksim Sychev <maksim_sychev@epam.com>
2022-03-21 15:57:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,20 @@ const cors = require("cors");
 const UserRouter = require('./router/user-router');
 const StaffRouter = require('./router/staff-router');
 const {errors} = require('celebrate')
-const cookieParser = require("cookie-parser");
 const bearerToken = require('express-bearer-token');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(cookieParser());
 app.use(bearerToken());
-
-app.use(UserRouter);
-app.use(StaffRouter);
-
 app.use(cors());
 
 app.use(errors())
+
+
+app.use(UserRouter);
+app.use(StaffRouter);
 
 const PORT = process.env.PORT || 3000;
 
